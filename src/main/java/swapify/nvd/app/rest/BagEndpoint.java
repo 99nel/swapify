1378527fe550992962117e38a124c7d8dd31e7b7
99nel <nvdoorn@gmail.com>
2022-08-26 12:00:29 +0200

    21a

--- src/main/java/swapify/nvd/app/rest/BagEndpoint.java
+++ src/main/java/swapify/nvd/app/rest/BagEndpoint.java
@@ -1,22 +1,42 @@
 package swapify.nvd.app.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import swapify.nvd.app.model.Bag;
+import swapify.nvd.app.persistence.BagService;
 
 @RestController
+@RequestMapping("api/bag")
 public class BagEndpoint {
 	
-	@GetMapping("/bagtest")
-	public void test() {
+	@Autowired
+	private BagService bs;
+	
+	@GetMapping("/:Id")
+	public void getBagById(@PathVariable("Id")long Id) {
 		System.out.println("I'm a pickle Pickleriiiiick");
 	}
 	
-	@PostMapping("/saveBag")
+	@PostMapping
 	public void saveBag(@RequestBody Bag newBag) {
+		bs.saveBag(newBag);
 		System.out.println(newBag);
 	}
+	
+	@GetMapping
+	public List<Bag> getBagAll() {
+		bs.getBagAll();
+		System.out.println("alle tassen verzamelen is gelukt!");
+		return bs.getBagAll();
+
+	}
+	
 }
